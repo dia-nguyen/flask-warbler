c9176c39e94622a6549545507d8a7f791323a8c9
Diane <hello@dianenguyen.ca>
2022-12-15 11:15:11 -0800

    Add tests for User model

--- test_user_model.py
+++ test_user_model.py
@@ -9,6 +9,7 @@ import os
 from unittest import TestCase
 
 from models import db, User, Message, Follows, connect_db
+from sqlalchemy import exc
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -53,3 +54,60 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u1.messages), 0)
         self.assertEqual(len(u1.followers), 0)
+
+    def test_is_following(self):
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u2_id)
+
+        u1.following.append(u2)
+        db.session.commit()
+
+        self.assertTrue(u1.is_following(u2))
+        self.assertFalse(u2.is_following(u1))
+
+    def test_is_followed_by(self):
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u2_id)
+
+        u1.following.append(u2)
+        db.session.commit()
+
+        self.assertTrue(u2.is_followed_by(u1))
+        self.assertFalse(u1.is_followed_by(u2))
+
+    def test_successful_user_signup(self):
+        new_user = User.signup("new_user", "new_user@email.com", "password", None)
+
+        db.session.commit()
+        self.assertEqual(new_user.username, "new_user")
+        self.assertEqual(new_user.email, "new_user@email.com")
+        self.assertIn("$2b$12$", new_user.password)
+
+    def test_unsuccessful_user_signup(self):
+        User.signup("u1", "new_user@email.com", "password", None)
+        with self.assertRaises(exc.IntegrityError):
+            db.session.commit()
+
+    def test_valid_credentials(self):
+        u1 = User.query.get(self.u1_id)
+        valid_credentials = User.authenticate("u1", "password")
+
+        self.assertEqual(valid_credentials, u1)
+
+    def test_invalid_username(self):
+        invalid_credentials = User.authenticate("u3", "password")
+        self.assertFalse(invalid_credentials)
+
+    def test_invalid_password(self):
+        invalid_credentials = User.authenticate("u1", "wrongpassword")
+        self.assertFalse(invalid_credentials)
+
+
+
+
+
+
+
+
+
+
