222bc8952ee1af12339ce4f9ac5d633f005c273b
Diane <hello@dianenguyen.ca>
2022-12-15 15:40:04 -0800

    Add test for user views

--- test_user_views.py
+++ test_user_views.py
@@ -7,8 +7,9 @@
 
 import os
 from unittest import TestCase
-
+from flask import session
 from models import db, Message, User, connect_db
+from app import CURR_USER_KEY
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -53,7 +54,10 @@ class UserViewTestCase(TestCase):
 
         self.client = app.test_client()
 
-    def test_display_sign_up(self):
+    def teardown(self):
+        db.session.rollback()
+
+    def test_display_signup_form(self):
         with app.test_client() as client:
             resp = client.get("/signup")
             html = resp.get_data(as_text=True)
@@ -61,7 +65,7 @@ class UserViewTestCase(TestCase):
             self.assertEqual(resp.status_code, 200)
             self.assertIn('Sign me up!', html)
 
-    def test_sign_up(self):
+    def test_signup(self):
         with app.test_client() as client:
             resp = client.post("/signup",
             data={
@@ -75,21 +79,146 @@ class UserViewTestCase(TestCase):
             self.assertEqual(resp.status_code, 200)
             self.assertIn("@test_user", html)
 
-    def test_invalid_sign_up(self):
+    #TODO: invalid sign up
+    # def test_invalid_sign_up(self):
+    #     with app.test_client() as client:
+    #         resp = client.post("/signup",
+    #         data={
+    #             'username': 'u1',
+    #             'email': 'test@email.com',
+    #             'password': 'testpassword',
+    #         },
+    #         follow_redirects=True)
+    #         html = resp.get_data(as_text=True)
+
+    #         self.assertEqual(resp.status_code, 200)
+    #         # check to see if still on sign up form
+    #         self.assertIn('Sign me up!', html)
+    #         self.assertIn("Username already taken", html)
+
+    def test_display_login_form(self):
         with app.test_client() as client:
-            resp = client.post("/signup",
+            resp = client.get("/login")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Welcome back.', html)
+
+    def test_login(self):
+        with app.test_client() as client:
+            resp = client.post("/login",
             data={
                 'username': 'u1',
-                'email': 'test@email.com',
-                'password': 'testpassword',
+                'password': 'password',
             },
             follow_redirects=True)
             html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
-            # check to see if still on sign up form
-            self.assertIn('Sign me up!', html)
-            self.assertIn("Username already taken", html)
+            self.assertIn("Hello, u1!", html)
+
+
+
+
+class UserViewLoggedInTestCase(TestCase):
+    def setUp(self):
+        User.query.delete()
+
+        u1 = User.signup("u1", "u1@email.com", "password", None)
+        u2 = User.signup("u2", "u2@email.com", "password", None)
+        u3 = User.signup("u3", "u3@email.com", "password", None)
+
+        db.session.add_all([u1, u2, u3])
+        u1.following.append(u2)
+        u3.following.append(u1)
+
+        db.session.commit()
+
+        self.u1_id = u1.id
+        self.u2_id = u2.id
+        self.u3_id = u3.id
+
+        self.client = app.test_client()
+
+    def teardown(self):
+        db.session.rollback()
+
+    def test_logout(self):
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1_id
+
+            resp = c.post("/logout",
+            follow_redirects=True)
+
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("Good bye!", html)
+
+    def test_display_all_users(self):
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1_id
+
+            resp = c.get("/users")
+
+            html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("u1", html)
+            self.assertIn("u2", html)
+            self.assertIn("u3", html)
+
+    def test_display_specific_user(self):
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1_id
+
+            resp = c.get("/users?=u1")
+
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("u1", html)
+            self.assertNotIn("u2", html)
+            self.assertNotIn("u3", html)
+
+    def test_display_user_profile(self):
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1_id
+
+            resp = c.get("/users/u1")
+
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("@u1", html)
+            self.assertIn("warbler-hero", html)
+
+
+    def test_display_following(self):
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1_id
+
+            resp = c.get("/users/u1/following")
+
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("@u2", html)
+            self.assertIn("Unfollow", html)
+
+    def test_display_followers(self):
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1_id
+
+            resp = c.get("/users/u1/followers")
+
+            html = resp.get_data(as_text=True)
+            self.assertIn("@u3", html)
 
 
 
